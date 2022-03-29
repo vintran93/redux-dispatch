2bf5e09951b3d79a3cd95ea3cec175527655d882
Vincent Tran <vincentuong@yahoo.com>
2022-03-28 22:19:41 -0500

    commit tree af42235a700dc34a8e064127650f44d9dce81381

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,21 @@
-function changeState(state, action){
+let state = { count: 0 };
+
+function changeState(state, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action)
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+render();
